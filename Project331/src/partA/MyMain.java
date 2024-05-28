5446ca825a897af24db86c48e596bc8d18ec645f
mamadduh <123499558+mamadduh@users.noreply.github.com>
2024-05-28 10:31:09 +0400

    Update MyMain.java

--- Project331/src/partA/MyMain.java
+++ Project331/src/partA/MyMain.java
@@ -1,7 +1,4 @@
-/**
- * 
- */
-package partA;
+package PartA;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -9,133 +6,90 @@ import java.io.File;
 import java.io.IOException;
 import java.awt.image.DataBufferByte;
 
-/**
- * @author 7417834
- *
- */
 public class MyMain {
-	
-	public static short [][] grayImage;
-    public static int width;
-    public static int height;
-    private static BufferedImage image;
 
+public static short[][] grayImage;
+public static int width;
+public static int height;
+private static BufferedImage image;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String fileNameInp="TenCardG.jpg";
-	    String fileNameOut="TenCardG4.jpg";
-	    readColourImage(fileNameInp);
-	    
-	    short xCoord=100;
-        short yCoord=100;
-        short rectWidth=100;
-        short rectHeight=100;
-        int heightS;
-        int widthS;
-        short[][] Source;
-        short[][] Template;
-
-        
-        writeColourImage(fileNameOut,xCoord,yCoord,rectWidth,rectHeight);
-        
-        System.out.println(">> Completed! Check the rectangle at the left top corner of the generated "+ fileNameOut+ "image under this project folderr");
- 
-
-	}
+public static void main(String[] args){
+String fileNameInp="TenCardG.jpg";
+String fileNameOut="Template.jpg";
 
 
 
-public static void readColourImage(String fileName) {
-    
-    try
-    {
-     // RGB pixel values
-     byte[] pixels;
 
-     File inp=new File("TenCardG.jpg");
-     image = ImageIO.read(inp);
-     width = image.getWidth();
-     height = image.getHeight();          
-    
-     
-     pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-     System.out.println("Dimension of the image: WxH= " + width + " x "+height+" "+ "| num of pixels: "+ pixels.length);
- 
- 
-
-     //rgb2gray in a 2D array grayImage                 
-     int pr;// red
-     int pg;//  green
-     int pb;// blue     
-
-    grayImage =new short [height][width];
-    int coord;
-    for (int i=0; i<height;i++)
- 	   for(int j=0;j<width;j++)
- 	   {        		     
- 		   coord= 3*(i*width+j);
- 		   pr= ((short) pixels[coord] & 0xff); // red
-            pg= (((short) pixels[coord+1] & 0xff));//  green
-            pb= (((short) pixels[coord+2] & 0xff));// blue                
-            
- 		   grayImage[i][j]=(short)Math.round(0.299 *pr + 0.587 * pg  + 0.114 * pb);         
- 		   
- 	   }  
-    }
-    catch (IOException e) {
-        e.printStackTrace();
-        } 
-
-}
-
-
-
-/**
-* 
-* @param fileName
-* @param xCoord
-* @param yCoord
-* @param rectWidth
-* @param rectHeight
- * @return 
-*/
-public static short[][] writeColourImage(String fileName,short xCoord, short yCoord, short rectWidth, short rectHeight) {   
-try {                   
-
- Image scaledImage = image.getScaledInstance(-1,-1, 0);
- // rectangle coordinates and dimension to superimpose on the image
- ImageIO.write(
-         add_Rectangle(scaledImage,xCoord,yCoord,rectWidth,rectHeight),
-         "jpg",
-         new File(fileName));
-
+File inp=new File(fileNameInp);
+try {
+image = ImageIO.read(inp);
 } catch (IOException e) {
-  e.printStackTrace();
-  }      
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+int width_o = image.getWidth();
+int height_o = image.getHeight();
+int tempSize = width_o*height_o;
+System.out.println(fileNameInp + " width: " + width_o + " and height:" + height_o + "\nThe temp size is "+ tempSize);
+
+
+File inp1=new File(fileNameOut);
+try {
+image = ImageIO.read(inp1);
+} catch (IOException e) {
+e.printStackTrace();
+}
+width = image.getWidth();
+height = image.getHeight();
+int tempSize1 = width*height;
+System.out.println(fileNameOut + " width: " + width + " and height:" + height + "\nThe temp size is "+ tempSize1);
+
+
+readColourImage(fileNameOut);
+readColourImage(fileNameInp);
+}
+public static short[][] readColourImage(String fileName) {
+
+try
+{
+// RGB pixel values
+byte[] pixels;
+
+File inp=new File(fileName);
+image = ImageIO.read(inp);
+width = image.getWidth();
+height = image.getHeight();
+
+
+pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+System.out.println("Dimension of the " + fileName +" image: WxH= " + width + " x "+height+" "+ "| num of pixels: "+ pixels.length);
+
+
+
+//rgb2gray in a 2D array grayImage
+int pr;// red
+int pg;// green
+int pb;// blue
+
+grayImage =new short [height][width];
+int coord;
+for (int i=0; i for(int j=0;j {
+coord= 3*(i*width+j);
+pr= ((short) pixels[coord] & 0xff); // red
+pg= (((short) pixels[coord+1] & 0xff));// green
+pb= (((short) pixels[coord+2] & 0xff));// blue
+
+grayImage[i][j]=(short)Math.round(0.299 *pr + 0.587 * pg + 0.114 * pb);
+
+}
+}
+catch (IOException e) {
+e.printStackTrace();
+}
 return grayImage;
+
+
 }
 
-public static BufferedImage add_Rectangle(Image img, short xCoord, short yCoord, short rectWidth, short rectHeight) {
-
-    if (img instanceof BufferedImage) {
-        return (BufferedImage) img;
-    }
-
-    // Create a buffered image with transparency
-    BufferedImage bi = new BufferedImage(
-            img.getWidth(null), img.getHeight(null),
-            BufferedImage.TYPE_INT_RGB);
-
-    Graphics2D g2D = bi.createGraphics();
-    g2D.drawImage(img, 0, 0, null);
-    g2D.setColor(Color.RED);
-    g2D.drawRect(xCoord, yCoord, rectWidth, rectHeight);              
-    g2D.dispose();
-    return bi;
-}
 
 }
